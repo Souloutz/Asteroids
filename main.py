d8889c0b4bec7dab3e0db580f7b9eb15d80a5f30
Howard Kong <howardkong826@gmail.com>
2025-01-01 23:53:45 -0500

    Add background, player lives, and respawning

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
     clock = pygame.time.Clock()
     delta_time = 0 # time since last frame drawn
 
+    background_image:pygame.Surface = pygame.image.load("background.jpg").convert_alpha()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -27,7 +29,7 @@ def main():
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
 
-    while (True):
+    while (player.get_lives() != 0):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -38,9 +40,8 @@ def main():
         for asteroid in asteroids:
             collided:bool = asteroid.check_collision(player)
 
-            if (collided):
-                print("Game Over!")
-                return
+            if (collided and player.get_respawn_timer() <= 0):
+                player = player.respawn()
             
             for shot in shots:
                 collided:bool = shot.check_collision(asteroid)
@@ -49,13 +50,17 @@ def main():
                     shot.kill()
                     asteroid.split()
                 
-        screen.fill("black")
+        # screen.fill("black")
+        screen.blit(background_image, (0, 0))
 
         for element in drawable:
             element.draw(screen)
         
         pygame.display.flip() # refreshes the screen
         delta_time = clock.tick(60) / 1000 # limit framemate to 60 FPS
+
+    print("Game Over!")
+    pygame.quit()
    
 if (__name__ == "__main__"):
     main()
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_COOLDOWN, SHOT_RADIUS, SHOT_SPEED
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_LIVES, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_COOLDOWN, PLAYER_RESPAWN_COOLDOWN, SHOT_RADIUS, SHOT_SPEED
 from circleshape import CircleShape
 from shot import Shot
 
@@ -15,9 +15,20 @@ class Player(CircleShape):
         rotation: int - degree of rotation (out of 360)
             default to 180 (facing upwards)
         timer: int - timer until player can shoot again
+        respawn_timer: int - timer after being respawned where the player is invincible
+        lives: int - number of lives a player has
         
     Methods
     -------
+        get_respawn_timer() -> int
+            Return the player's respawn timer
+
+        get_lives() -> int
+            Return the number of lives remaining
+
+        lose_life() -> None
+            Reduces a player's life    
+    
         triangle() -> list
             Returns the three points of the player triangle
 
@@ -35,13 +46,24 @@ class Player(CircleShape):
 
         shoot() -> None
             Creates a new shot
+
+        respawn() -> Player
+            Respawns the player, resetting their respawn timer and lowering their lives
     """
 
     def __init__(self, x:int, y:int, rotation:int=180):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = rotation
         self.timer = 0
+        self.respawn_timer = 0
+        self.lives = PLAYER_LIVES
 
+    def get_respawn_timer(self) -> int:
+        return self.respawn_timer
+    
+    def get_lives(self) -> int:
+        return self.lives
+    
     def triangle(self) -> list:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -63,21 +85,32 @@ class Player(CircleShape):
     def update(self, delta_time: int) -> None:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            self.rotate(-delta_time)
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            self.rotate(delta_time)
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.move(delta_time)
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.move(-delta_time)
-        if keys[pygame.K_SPACE]:
-            if self.timer <= 0:
-                self.shoot()
+        if (self.respawn_timer <= 0):
+            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                self.rotate(-delta_time)
+            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                self.rotate(delta_time)
+            if keys[pygame.K_w] or keys[pygame.K_UP]:
+                self.move(delta_time)
+            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                self.move(-delta_time)
+            if keys[pygame.K_SPACE]:
+                if self.timer <= 0:
+                    self.shoot()
 
         self.timer -= delta_time
+        self.respawn_timer -= delta_time
 
     def shoot(self) -> None:
         self.timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.triangle()[0], self.position[1], SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * SHOT_SPEED
+
+    def respawn(self):
+        lives:int = self.lives
+        self.kill()
+
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 180)
+        player.respawn_timer = PLAYER_RESPAWN_COOLDOWN
+        player.lives = lives - 1
+        return player
